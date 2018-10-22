47a805f33f88ef41eee7b3ce0f0098f6bafef87e
DavideTriso <davidetrisolinI@yahoo.it>
2018-10-22 19:42:46 +0200

    Updated dependencies + fix colum grid for IE 11

--- gulpfile.js
+++ gulpfile.js
@@ -10,14 +10,14 @@ var gulp = require('gulp'),
 
 //SCSS -> CSS
 //Compile SCSS and save compiled + minified CSS file to 'dist' folder
-gulp.task('scssDist', function(done) {
-  return sass('src/**/*.scss', {style: 'expanded'}).pipe(autoprefixer('last 5 version')).pipe(gulp.dest('dist')).pipe(rename({suffix: '.min'})).pipe(cssnano()).pipe(gulp.dest('dist')).pipe(notify({message: 'scssDist task complete'}));
+gulp.task('scssDist', function (done) {
+  sass('src/**/*.scss', { style: 'expanded' }).pipe(autoprefixer('last 5 version')).pipe(gulp.dest('dist')).pipe(rename({ suffix: '.min' })).pipe(cssnano()).pipe(gulp.dest('dist')).pipe(notify({ message: 'scssDist task complete' }));
   done();
 });
 
 //Save a copy of compiled CSS in 'docs' folder
-gulp.task('scssDocs', function(done) {
-  return sass('src/**/*.scss', {style: 'expanded'}).pipe(autoprefixer('last 5 version')).pipe(gulp.dest('docs')).pipe(rename({suffix: '.min'})).pipe(notify({message: 'scssDocs task complete'}));
+gulp.task('scssDocs', function (done) {
+  sass('src/**/*.scss', { style: 'expanded' }).pipe(autoprefixer('last 5 version')).pipe(gulp.dest('docs')).pipe(rename({ suffix: '.min' })).pipe(notify({ message: 'scssDocs task complete' }));
   done();
 });
 
@@ -26,6 +26,6 @@ gulp.task('scss', gulp.parallel('scssDist', 'scssDocs'));
 
 //WATCH TASKS
 
-gulp.task('watchScss', function() {
+gulp.task('watchScss', function () {
   gulp.watch('src/**/*.scss', gulp.parallel('scss'));
 });
